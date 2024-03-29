5795a5f824f46c65201920af4ac78251892bcf7b
Jay Mellor <jaymellor@Jays-MacBook-Pro.local>
2024-03-29 19:36:04 +0000

    bifurcation calculations

--- nums.lisp
+++ nums.lisp
@@ -1,4 +1,8 @@
-(defparameter *k* 3 "'Dial' constant")
+
+;; Probably want
+;; (setq *read-default-float-format* 'double-float)
+
+(defparameter *k* 3.0 "'Dial' constant")
 
 (defun next-step (input)
   "x_n+1 = kx_n(1 - x_n)"
@@ -10,10 +14,10 @@
 		(k (if k-specified? k-val *k*)))
 	(format t "For x0 = ~A and k = ~A~%" x0 k)
 	(format t "~,5f~%" x0)
-	(let ((results (interation-results input :steps steps :k-val k)))
+	(let ((results (interaction-results input :steps steps :k-val k)))
 	  (format t "~{~,5f~^~%~}" results))))
 
-(defun interation-results (input &key (steps 10)
+(defun interaction-results (input &key (steps 10)
 								   (k-val 0 k-specified?)
 								   (step-fn #'next-step))
   (let ((results nil))
@@ -23,3 +27,11 @@
 		(setf results (cons output results))
         (setf input output)))
 	(reverse results)))
+
+(defun bifurcation-diagram (&key (x0 0.3) (steps 10))
+  (loop for *k* from 0 to 4.2 by 0.2
+		for result = (interaction-results x0 :k-val *k* :steps steps)
+		collect result))
+
+;; (loop for result in (bifurcation-diagram :steps 100)
+;; 	  collect (nthcdr 80 result))
